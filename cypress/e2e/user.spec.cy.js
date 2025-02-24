65a9ceacbd1ac9b6f30eba20d2ac355ddcd48b1b
athaynapontes <thaynap67@gmail.com>
2025-02-24 15:34:48 -0300

    User update scenario added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -9,18 +9,36 @@ const selectorsList = {
   loginButton: "[type='submit']",
   sectionTittleTopBar: ".oxd-topbar-header-breadcrumb-module",
   dashboardGrid: ".orangehrm-dashboard-grid",
-  wrongCredencialAlert: "[role='alert']"
+  wrongCredencialAlert: "[role='alert']",
+  myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+  firstNameField: "[name='firstName']",
+  lastNameField: "[name='lastName']",
+  genericField: ".oxd-input--active",
+  dateField: "[placeholder='yyyy-dd-mm']",
+  dateCloseButton: ".--close",
+  submitButton: "[type='submit']"
 
 }
 
 
-  it('login - sucess', () => {
+  it.only('User Info Update - Sucess', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField). type (userData.userSucess.username)
     cy.get(selectorsList.passwordField). type (userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton) . click()
+    cy.get(selectorsList.firstNameField).clear().type("FirstNameTest")
+    cy.get(selectorsList.lastNameField).clear().type("LastNameTest")
+    cy.get(selectorsList.genericField).eq(3).clear().type("EmployeeId")
+    cy.get(selectorsList.genericField).eq(4).clear().type("OtherIdTest")
+    cy.get(selectorsList.genericField).eq(5).clear().type("DriversLicenceTest")
+    cy.get(selectorsList.dateField).eq(0).clear().type("2150-03-10")
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('.oxd-toast-close')
+  
   })
   it('login - fail', () => {
     cy.visit('/auth/login')
